c49d5e9daef85c4c4e77aa5c17013192589955a4
Andy <andy.guo@waclighting.com.cn>
2022-01-24 10:41:29 +0800

    1.Supported click the phone item in letter page to skip to phone info page. 2.Released the version 1.0.0rc3.

--- app/src/main/java/com/phone/book/fragment/home/HunFamilyFragment.kt
+++ app/src/main/java/com/phone/book/fragment/home/HunFamilyFragment.kt
@@ -14,9 +14,11 @@ import com.phone.book.common.adapter.CustomBaseAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.phone.book.GridItemDivider
+import com.phone.book.activity.EditInfoContainerActivity
 import com.phone.book.adapter.SingleSelectAdapter2
 import com.phone.book.bean.HunLetterBean
 import com.phone.book.bean.PhoneBookItem
+import com.phone.book.common.listener.OnSingleClickListener
 import com.phone.book.manager.PhoneInfoManager
 import com.phone.book.utils.PinyinUtils
 import kotlinx.android.synthetic.main.fragment_hun_family.*
@@ -143,6 +145,12 @@ class HunFamilyFragment : BaseFragment() {
         override fun onBindCustomViewHolder(holder: PhoneListHolder, phoneBookItem: PhoneBookItem) {
             holder.tvName.text = phoneBookItem.name
             holder.tvNumber.text = if (phoneBookItem.extension1.isNotEmpty()) phoneBookItem.extension1 else if (phoneBookItem.extension2.isNotEmpty()) phoneBookItem.extension2 else ""
+            holder.itemView.setOnClickListener(object : OnSingleClickListener() {
+                override fun onSingleClick(v: View) {
+                    EditInfoContainerActivity.startPhoneInfoPage(mMainActivity, phoneBookItem)
+                }
+
+            })
         }
 
         inner class PhoneListHolder(itemView: View) : CustomBaseHolder(itemView) {
